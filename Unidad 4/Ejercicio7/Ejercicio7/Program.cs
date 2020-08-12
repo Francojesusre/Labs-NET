cd1cbb86e247dd92d7e131e9bd4d455f835482a7
Franco <francolosmo7@gmail.com>
2020-08-12 13:17:50 -0300

    lab 04

--- Unidad 4/Ejercicio7/Ejercicio7/Program.cs
+++ Unidad 4/Ejercicio7/Ejercicio7/Program.cs
@@ -17,9 +17,6 @@ namespace Ejercicio7
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-
-
-            //
         }
     }
 }
